3a49ffd0d061edd5de96004ca98dcbb2ec4251df
SameerArjunS <112655999+Sameer-Arjun-S@users.noreply.github.com>
2023-10-24 15:58:54 -0400

    Delete build directory

--- build/test/cpp-test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/sas/Ackermann-steering-controller-ENPM808X/build/test/cpp-test[1]_tests.cmake")
-  include("/home/sas/Ackermann-steering-controller-ENPM808X/build/test/cpp-test[1]_tests.cmake")
-else()
-  add_test(cpp-test_NOT_BUILT cpp-test_NOT_BUILT)
-endif()
