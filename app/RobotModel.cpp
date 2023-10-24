e1faff8cfbe3cf677fced5cde1ec08f3bc96fd0d
SameerArjunS <112655999+Sameer-Arjun-S@users.noreply.github.com>
2023-10-24 14:17:17 -0400

    Update RobotModel.cpp

--- app/RobotModel.cpp
+++ app/RobotModel.cpp
@@ -1,5 +1,16 @@
+/**
+ * @file RobotModel.cpp
+ * @author Driver - Sameer Arjun S
+           Navigator -  Ishaan Samir Parikh
+           Design Keeper - Manav Nagda
+ * @brief Represents a class to implement the robot model
+ * @version 0.1
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #include "RobotModel.hpp"
-#include <cmath>ss
+#include <cmath>
 
 RobotModel::RobotModel(double wheelbase, double trackWidth, double maxSteeringAngle)
     : wheelbase_(wheelbase), trackWidth_(trackWidth), maxSteeringAngle_(maxSteeringAngle),
@@ -7,17 +18,47 @@ RobotModel::RobotModel(double wheelbase, double trackWidth, double maxSteeringAn
 }
 
 void RobotModel::setInitialState(double x, double y, double theta, double velocity) {
-    // Stub implementation
+    x_ = x;
+    y_ = y;
+    theta_ = theta;
+    velocity_ = velocity;
 }
 
 void RobotModel::updateState(double steeringAngle, double dt) {
-    // Stub implementation
+    // Apply the Ackermann kinematic model with a maximum steering angle constraint.
+    if (std::abs(steeringAngle) > maxSteeringAngle_) {
+        // Limit the steering angle to the maximum allowed.
+        steeringAngle = std::copysign(maxSteeringAngle_, steeringAngle);
+    }
+
+    // Calculate left and right wheel velocities based on the steering angle and velocity.
+    double turningRadius = wheelbase_ / std::tan(steeringAngle);
+    double leftWheelVelocity = velocity_ * (turningRadius - (trackWidth_ / 2.0)) / turningRadius;
+    double rightWheelVelocity = velocity_ * (turningRadius + (trackWidth_ / 2.0)) / turningRadius;
+
+    // Update the robot's state.
+    double deltaTheta = (leftWheelVelocity - rightWheelVelocity) / trackWidth_ * dt;
+    double deltaX = 0.5 * (leftWheelVelocity + rightWheelVelocity) * std::cos(theta_) * dt;
+    double deltaY = 0.5 * (leftWheelVelocity + rightWheelVelocity) * std::sin(theta_) * dt;
+
+    x_ += deltaX;
+    y_ += deltaY;
+    theta_ += deltaTheta;
 }
 
 void RobotModel::calculateSteeringAndDriveVelocities(double desiredTurningRadius, double& steeringAngle, double& leftWheelVelocity, double& rightWheelVelocity) {
-    // Stub implementation
+    // Calculate the steering angle based on the desired turning radius and Ackermann kinematic model.
+    steeringAngle = std::atan(wheelbase_ / desiredTurningRadius);
+
+    // Calculate left and right wheel velocities for the Ackermann model.
+    double turningRadius = wheelbase_ / std::tan(steeringAngle);
+    leftWheelVelocity = velocity_ * (turningRadius - (trackWidth_ / 2.0)) / turningRadius;
+    rightWheelVelocity = velocity_ * (turningRadius + (trackWidth_ / 2.0)) / turningRadius;
 }
 
 void RobotModel::getState(double& x, double& y, double& theta, double& velocity) {
-    // Stub implementation
+    x = x_;
+    y = y_;
+    theta = theta_;
+    velocity = velocity_;
 }
