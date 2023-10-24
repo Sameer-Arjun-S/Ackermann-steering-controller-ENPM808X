6b495b6390f0f34ed1441b41d3283298b063ecc0
SameerArjunS <112655999+Sameer-Arjun-S@users.noreply.github.com>
2023-10-24 15:36:20 -0400

    Updated code coverage
    Resolved errors in Codecov file

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/sas/Ackermann-steering-controller-ENPM808X
+# Build directory: /home/sas/Ackermann-steering-controller-ENPM808X/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("_deps/googletest-build")
+subdirs("app")
+subdirs("test")
---
+++ build/test/CMakeFiles/cpp-test.dir/test.cpp.o.d
@@ -0,0 +1,3 @@
+test/CMakeFiles/cpp-test.dir/test.cpp.o: \
+ /home/sas/Ackermann-steering-controller-ENPM808X/test/test.cpp \
+ /usr/include/stdc-predef.h
