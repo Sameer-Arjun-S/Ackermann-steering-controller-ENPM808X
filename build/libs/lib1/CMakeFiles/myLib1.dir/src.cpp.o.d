5c6a2ea98ec104a46aa34b326fd7e97f040b00c6
SameerArjunS <112655999+Sameer-Arjun-S@users.noreply.github.com>
2023-10-24 15:16:45 -0400

    Added Codecov
    Made changes to rectify errors in Codecov file

--- build/libs/lib1/CMakeFiles/myLib1.dir/src.cpp.o.d
+++
@@ -1,2 +0,0 @@
-libs/lib1/CMakeFiles/myLib1.dir/src.cpp.o: \
- /home/sas/midterm/libs/lib1/src.cpp /usr/include/stdc-predef.h
--- build/libs/lib2/CMakeFiles/myLib2.dir/src.cpp.o.d
+++
@@ -1,2 +0,0 @@
-libs/lib2/CMakeFiles/myLib2.dir/src.cpp.o: \
- /home/sas/midterm/libs/lib2/src.cpp /usr/include/stdc-predef.h
