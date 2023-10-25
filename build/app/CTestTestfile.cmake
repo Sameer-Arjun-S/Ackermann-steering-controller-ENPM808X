48f43503a082018418e288ed6c7a920d8771f4eb
SameerArjunS <112655999+Sameer-Arjun-S@users.noreply.github.com>
2023-10-24 21:05:11 -0400

    remove build files

--- build/app/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sas/Ackermann-steering-controller-ENPM808X/app
-# Build directory: /home/sas/Ackermann-steering-controller-ENPM808X/build/app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
