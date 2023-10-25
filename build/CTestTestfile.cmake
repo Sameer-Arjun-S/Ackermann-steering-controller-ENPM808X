67770f7297723d20d5073ff8b11adbe9022944cc
SameerArjunS <112655999+Sameer-Arjun-S@users.noreply.github.com>
2023-10-24 20:16:28 -0400

    changed CmakeLists for codecov
    changed cmakelists for codcov report generation on github

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sas/Ackermann-steering-controller-ENPM808X
-# Build directory: /home/sas/Ackermann-steering-controller-ENPM808X/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
-subdirs("app")
-subdirs("test")
